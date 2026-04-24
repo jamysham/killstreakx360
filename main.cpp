7241db815da9d605fb9f248424dbcb9b44b6a00d
jamysham <ragetails68@gmail.com>
2026-04-24 09:58:20 -0500

    No point if it's not released

--- main.cpp
+++ main.cpp
@@ -1,23 +1,5 @@
 /*
-This code is currently barely empty, not released.
+This code is currently  empty, not released.
 You could either wait for Cosmos Public Plugin to release.
 Thanks!
 */
-#include <xtl.h>
-
-void ShowNotification(const char* message)
-{
-    wchar_t wMessage[256];
-    mbstowcs(wMessage, message, strlen(message) + 1);
-
-    XNotifyQueueUI(
-        XNOTIFYUI_TYPE_PREFERRED_REVIEW,  
-        XUSER_INDEX_ANY,                  
-        XNOTIFYUI_PRIORITY_DEFAULT,       
-        wMessage,                          
-        NULL                               
-    );
-}
-
-
-ShowNotification("Killstreak plugin loading.");
