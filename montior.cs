145a2e2c02fcc2765c9db652b6baa99872c565d6
Chefy <94152428+taillsangelYT@users.noreply.github.com>
2026-04-24 13:28:16 -0500

    Create montior.cs

---
+++ montior.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Magic Conch";
+            Console.WriteLine("Hi, We love easter eggs"); // Make it say the Line in command console
+            Console.WriteLine("Thanks for sticking out!");
+            Console.WriteLine("This code is gonna 'montior' the killstreak plugin");
+            Console.Read();
+        }
+    }
+}
